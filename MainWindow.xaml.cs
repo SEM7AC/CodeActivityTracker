4784beda9469634b68494ab0ea75c5dfbd6211ca
Christopher S Semler <semler.dev@outlook.com>
2026-08-09 12:41:28 -0700

    Code cleanup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,30 +1,15 @@
 ﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
-using System.Runtime.InteropServices;
 
 namespace CodeActivityTracker
     {
     public partial class MainWindow : Window
         {
-        
-
-        // Win32API for capturing Idle time
-        [DllImport("user32.dll")]
-        static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct LASTINPUTINFO
-            {
-            public uint cbSize;
-            public uint dwTime;
-            }
-
         public MainWindow()
             {
             InitializeComponent();
-            
-            
+
+
             }
 
         // WINDOW BAR AND DRAG METHODS
@@ -43,25 +28,6 @@ namespace CodeActivityTracker
             {
             WindowState = WindowState.Minimized;
             }
-
-        private string FormatTime(int seconds)
-            {
-            return TimeSpan.FromSeconds(seconds).ToString(@"hh\:mm\:ss");
-            }
-        
-        private int GetIdleTimeSeconds()
-            {
-            LASTINPUTINFO info = new LASTINPUTINFO();
-            info.cbSize = (uint)Marshal.SizeOf(info);
-
-            if (!GetLastInputInfo(ref info))
-                return 0;
-            
-            uint idleTime = ((uint)Environment.TickCount - info.dwTime);
-            return (int)(idleTime / 1000);
-            }
-
-
 
 
         }
